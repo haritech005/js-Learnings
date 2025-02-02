9251c57ab15bfb3720c8a3c3f708cf6c9ce38908
Hariharan Jeganathan <j.hariharan005@gmail.com>
2025-02-02 11:01:09 +0530

    new promise

--- JavaScript/promisechain.js
+++ JavaScript/promisechain.js
@@ -1,2 +1,30 @@
-console.log("promise chain");
-console.log("hello")
+// console.log("promise chain");
+// console.log("hello")
+
+const cart = ["chocolate","biscut","mangoes"];
+
+const promise = createOrder(cart)
+
+promise.then(function(orderId){
+    console.log(orderId);
+    
+})
+
+
+function createOrder(cart){
+    
+    const pr = new Promise (function(resolve,reject){
+        if(!validateCart(cart)){
+            let error = new Error("Cart is not valid");
+            reject(error);
+        } else {
+            const orderId = "12345"; // Simulating order ID
+            resolve(orderId);
+        }
+    })
+    return pr;
+}
+
+function validateCart(cart){
+    return true;
+}

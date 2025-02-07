51e7eb6897ea947329145a0d37e90cc115eb8899
hariharanj-01 <hariharanj@lumel.com>
2025-02-07 22:36:53 +0530

    async await

---
+++ JavaScript/AsyncAwait.js
@@ -0,0 +1,13 @@
+// console.log("async await");
+
+let p = new Promise((resolve,reject) =>{
+    resolve("Promise is resolved")
+})
+
+ function handlePromise(){
+    const data =  p
+    data.then((res) => console.log(res)
+    )
+    
+}
+handlePromise()
